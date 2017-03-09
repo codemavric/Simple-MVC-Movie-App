860215fc1bb7065f55ee960d2355b8644b1765c0
Vicram RJ Purshotam <vicram.purshotam@gmail.com>
2017-03-10 00:14:32 +0200

    Durban University of Technology - Application Development MVC exercise.

---
+++ MovieApp/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieApp.Models;
+
+namespace MovieApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        [HttpGet]
+        public ActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult AddMovie(Movie m)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.MovieDetails =
+                    $"Movie Title: {m.title}" + "<br/> <br/>" +
+                    $"Release Date: {m.releaseDate.ToString("MMMM yyyy")}" + "<br/> <br/>" +
+                    $"Genre: {m.genre}" + "<br/> </br>" +
+                    $"Price: {m.price.ToString("C")}" + "<br/> <br/>" +
+                    $"Is movie discounted:? {m.isMovieDiscounted()}";
+                return View("ShowMovie");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MovieApp/Models/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage ="Please enter in a valid movie title:")]
+        [StringLength(30)]
+        public string title { get; set; }
+
+        [Required(ErrorMessage ="Please enter in a valid date (MM/yyyy)")]
+        [DisplayFormat(DataFormatString ="{0:MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime releaseDate { get; set; }
+        public string genre { get; set; }
+        [Range(50,350)]
+        public double price { get; set; }
+        public bool onDiscount { get; set; }
+
+        public string isMovieDiscounted()
+        {
+            string answer = "";
+
+            if (onDiscount == true)
+            {
+                answer = "Yes";
+            }
+            else
+            {
+                answer = "No";
+            }
+            return answer;
+        }
+
+
+    }
+}
